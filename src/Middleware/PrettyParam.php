3aab9c796b025b86dc982dd8b98e53f500f56cee
Stephen Perelson <stephenp@oneafricamedia.com>
2017-11-18 21:45:04 +0200

    Actually prettify the GET parameters

--- src/Middleware/PrettyParam.php
+++ src/Middleware/PrettyParam.php
@@ -9,10 +9,34 @@ class PrettyParam
 
     public function handle(Request $request, \Closure $next)
     {
-    	var_dump($request->server('REQUEST_URI'));
-    	die;
+    	$query = $request->server('QUERY_STRING');
+
+        if (!empty($query)) {
+            $query = explode('&', $query);
+            $params = [];
+            $input = $request->input();
+
+            foreach ($query as $param) {
+                $item = explode('=', $param);
+
+                if (count($item) === 2) {
+                    $name = urldecode($item[0]);
+                    $value = urldecode($item[1]);
+
+                    if (substr($name, -2) !== '[]') {
+                        $params[$name][] = $value;
+                    }
+                }
+            }
+            foreach ($params as $key => $param) {
+                if (count($param) > 1) {
+                    $input[$key] = $param;
+                }
+            }
+
+	    	$request->merge($input);
+        }
 
         return $next($request);
     }
-
 }
